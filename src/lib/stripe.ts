671d8170238d39e69a952aaad1d8090a7d85c418
Erivaldo-Montes <erivaldomontez@gmail.com>
2022-12-31 22:27:57 -0300

    stripe api

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRETE_KEY, {
+export const stripe = new Stripe(process.env.STRIPE_SECRETE_KEY as string, {
   apiVersion: '2022-11-15',
   appInfo: {
     name: 'ignite shop',
